857c96b47c03e54902666321b0ecedb0fe5d40fd
Matt <mkeeney4202@gmail.com>
2024-10-10 18:44:51 -0400

    final commit

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,7 +4,16 @@ const User = require('../../models/User');
 // ALL of these of routes are prefixed with '/api/users'
 router.get('/', (req, res) => {
     // We want to QUERY our DATABASE for ALL USERS
-    User.find({}, (err, data) => {
+    User.find({})
+        .then(result => {
+            console.log("Data: ", result);
+            res.status(200).json(result);
+        }).catch(err => {
+            console.log("Error: ", err);
+            res.status(500).json({ msg: err })
+        });
+    /*
+    , (err, data) => {
         if(err) {
             console.log("err: ", err);
             res.status(500).json({ msg: err })
@@ -13,23 +22,24 @@ router.get('/', (req, res) => {
         // RESPONSE BACK (in JSON) to the REQUESTing entity
         res.status(200).json(data);
     })
-
+    */
 });
 
 
 router.get('/:userId', (req, res) => {
     // We can pull out the userId from the INCOMING REQUEST --> req.params = { userId: '098sf097w3ergwef098sd'}
-    User.findById(req.params.userId, (err, data) => {
-        if(err) {
+    User.findById(req.params.userId)
+        .then(data => {
+            // We MAY need to PARSE the data from the data base BEFORE passing it BACK 
+            console.log("DB data: ", data);
+            // response
+            res.status(201).json(data)
+        })
+        .catch(err => {
             console.log("error: ", err);
+            // response
             res.status(500).json({ msg: err })
-        }
-        console.log("User Found: ", data);
-        // We MAY need to PARSE the data from the data base BEFORE passing it BACK 
-        
-        // response
-        res.status(200).json(data)
-    })
+        });
 });
 
 router.post('/', (req, res) => {
@@ -38,49 +48,52 @@ router.post('/', (req, res) => {
     // WE want to CREATE a new Record in our dataabse
     const { username, email } = req.body
     // USING that userId we can search our DB for THAT user
-    User.create(req.body, (err, data) => {
-        if(err) {
+    User.create(req.body)
+        .then(data => {
+            // We MAY need to PARSE the data from the data base BEFORE passing it BACK 
+            console.log("DB data: ", data);
+            // response
+            res.status(201).json(data)
+        })
+        .catch(err => {
             console.log("error: ", err);
+            // response
             res.status(500).json({ msg: err })
-        }
-        console.log("DB data: ", data);
-        // We MAY need to PARSE the data from the data base BEFORE passing it BACK 
+        });
     
-        // response
-        res.status(201).json(data)
-    })
-
 });
 
+
 router.put('/:userId', (req, res) => {
+   // console.log("Params: ", req.params);
+  //  console.log("Body: ", req.body);
     // We can pull out the userId from the INCOMING REQUEST --> req.params = { userId: '098sf097w3ergwef098sd'}
-    User.findByIdAndUpdate(req.params.userId, req.body, (err, data) => {
-        if(err) {
+    User.findByIdAndUpdate(req.params.userId, req.body, { new: true })
+        .then(data =>{
+            console.log("User Updated: ", data);
+            res.status(200).json(data) 
+        }).catch(err => {
             console.log("error: ", err);
-            res.status(500).json({ msg: err })
-        }
-        console.log("User Updated: ", data);
-        // We MAY need to PARSE the data from the data base BEFORE passing it BACK 
-        
-        // response
-        res.status(200).json(data)
-    })
+            res.status(500).json({ msg: err.message })
+        });
 });
 
 router.delete('/:userId', (req, res) => {
     // We can pull out the userId from the INCOMING REQUEST --> req.params = { userId: '098sf097w3ergwef098sd'}
-    User.findByIdAndDelete(req.params.userId, (err, data) => {
-        if(err) {
+    User.findByIdAndDelete(req.params.userId) 
+        .then(data =>{
+            console.log("User Deleted: ", data);
+            res.status(200).json({ msg: "User Deleted Successfully"})
+            
+        }).catch(err => {
             console.log("error: ", err);
-            res.status(500).json({ msg: err })
-        }
-        console.log("User Deleted: ", data);
+            res.status(500).json({ msg: err.message })
+        });
+    });
         // We MAY need to PARSE the data from the data base BEFORE passing it BACK 
         
         // response
-        res.status(200).json({ msg: "User Deleted Successfully"})
-    })
-});
+    
 
 // --- Friend Routes --- //
 router.post('/:userId/friends/:friendId', (req, res) => {
@@ -89,18 +102,15 @@ router.post('/:userId/friends/:friendId', (req, res) => {
     
     User.findByIdAndUpdate(req.params.userId, 
         { $addToSet: { friends: req.params.friendId} },
-        { new: true },
-        (err, data) => {
-            if(err) {
-                console.log("error: ", err);
-                res.status(500).json({ msg: err })
-            }
+        { new: true })
+        .then(data => {
             console.log("User Updated: ", data);
-            // We MAY need to PARSE the data from the data base BEFORE passing it BACK 
-            
-            // response
-            res.status(200).json(data)
+            res.status(200).json(data);
+        }).catch(err => {
+            console.log("error: ", err);
+            res.status(500).json({ msg: err })
         });
+       
 });
 
 router.delete('/:userId/friends/:friendId', (req, res) => {
